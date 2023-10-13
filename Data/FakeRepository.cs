68010b8bbc06a2597098d424e8574a22f63ba952
PINELA <pinelaivo10@gmail.com>
2023-10-13 12:06:15 +0100

    Add a fake repository

---
+++ Data/FakeRepository.cs
@@ -0,0 +1,36 @@
+﻿using SportsStore.Models;
+
+namespace SportsStore.Data
+{
+    public class FakeRepository : ISportsStoreRepository
+    {
+        public IEnumerable<Product> Products =>
+            new List<Product>
+            {
+                new Product
+                {
+                    ProductName="Football ball",
+                    ProductPrice= 25
+                },
+
+                new Product
+                {
+                    ProductName="SurfBoard",
+                    ProductPrice= 375
+                },
+
+                new Product
+                {
+                    ProductName="Basketball ball",
+                    ProductPrice= 45
+                },
+
+                new Product
+                {
+                    ProductName="Running Shoes",
+                    ProductPrice= 95
+                }
+            };
+
+    }
+}

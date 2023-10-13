ccb44d3563fb9c393e21f33b26e4ebdb5528ea00
PINELA <pinelaivo10@gmail.com>
2023-10-13 11:56:52 +0100

    Add interface for the (Sport Store) repository

---
+++ Data/ISportsStoreRepository.cs
@@ -0,0 +1,11 @@
+﻿using SportsStore.Models;
+
+namespace SportsStore.Data
+{
+    public interface ISportsStoreRepository
+    {
+        public IEnumerable<Product>Products { get; }
+
+            
+    }
+}

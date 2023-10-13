2659b4c51bffbaa27e51919af5ab38d54757dbec
PINELA <pinelaivo10@gmail.com>
2023-10-13 11:47:13 +0100

    Add product Model

---
+++ Models/Product.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SportsStore.Models
+{
+    public class Product
+    {
+        public int ProductId { get; set; }
+
+        [Required]
+        [StringLength(100,MinimumLength =2)]
+        public required string ProductName { get; set; }
+
+        public string? ProductDescription { get; set; }
+
+
+        public string? ProductCategory { get; set; }
+        [StringLength(50, MinimumLength = 2)]
+
+        [Required]
+        public required decimal ProductPrice { get; set; }
+    }
+}
